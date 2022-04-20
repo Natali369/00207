e5c419d17edc0a1592530dae662f29086fe2a949
Natali369 <103065839+Natali369@users.noreply.github.com>
2022-04-20 16:06:50 +0300

    Update Yandexbot.js
    Исправила ошибки. Спасибо Игорю Монахову)

--- Yandexbot.js
+++ Yandexbot.js
@@ -4,26 +4,32 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Natalia Grigorieva
-// @match        https://www.yandex.ru/*
+// @match        https://yandex.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let keywords=["купля-продажа авто","каталог автомобилей","автомобили купить","б\у авто"];
-let keyword=keywords[getRandom(0,keywords.length)];
-let minisuggest__button=document.getElementsByClassName("mini-suggest__button")[0];
+let num=keywords.length;
+let randind=getRandom(0,num);
+let keyword=keywords[randind];
+let mbutton=document.getElementsByClassName("button mini-suggest__button")[0];
 let links=document.links;
 
-if(minisuggest__button!==undefined) {
-document.getElementByTeg("a").removeAttribute("_blank");
-document.getElementsById("text")[0].value = keyword;
-document.getElementsByClassName("mini-suggest__button")[0].click();
+if(mbutton!==undefined) {
+console.log(mbutton);
+document.getElementsByName("text")[0].value = keyword;
+setTimeout(clickBtn,2000);
+//document.getElementsByClassName("mini-suggest__button")[0].click();
 }else{
 for (let i=0; i<links.length; i++) {
     if (links[i].href.indexOf("auto.ru")!==-1) {
     let link=links[i]
     console.log("Нашел строку" +links[i]);
-    link.click();
+     window.location.replace(link);
+    //link.click();
+   // link.setAttribute('target', '_self');
+//link.click();
 break;
     }
     }
@@ -31,4 +37,7 @@ break;
 
     function getRandom(min,max) {
     return Math.floor(Math.random()*(max-min)+min);
+    }
+    function clickBtn() {
+    document.getElementsByClassName("button mini-suggest__button")[0].click();
     }
