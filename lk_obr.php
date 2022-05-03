2d3e8d642a0aef366b125ebee4a1d15b5e7f2be3
krekser37 <88906761+krekser37@users.noreply.github.com>
2022-05-03 19:24:04 +0300

    Update lk_obr.php

--- lk_obr.php
+++ lk_obr.php
@@ -1,5 +1,4 @@
-<?php
-session_start();
+<?php session_start();
 header('Content-Type: text/html; charset=utf-8');
 
 $mysqli = mysqli_connect("localhost", "mhuhzmvm_0207", "12345", "mhuhzmvm_0207");
@@ -11,6 +10,9 @@ $inputValue=$_POST['value'];
 $item=$_POST['item'];
 $id=$_SESSION['id'];
 $mysqli->query("UPDATE `users` SET `$item` = '$inputValue' WHERE `id` = '$id'");
+
+echo $inputValue;
+echo $item;
 
 $_SESSION[$item]=$inputValue;
 }
